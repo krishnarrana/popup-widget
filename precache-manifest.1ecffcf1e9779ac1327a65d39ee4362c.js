5eef5ede22741a5d7a92a90a7c01b91fb1ca2df8
unknown <krishna2121.rana@gmail.com>
2018-11-07 18:13:51 +0100

    Updates

---
+++ precache-manifest.1ecffcf1e9779ac1327a65d39ee4362c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "32b4cd66bd600c925ddf",
+    "url": "/popup-widget/static/js/runtime~main.32b4cd66.js"
+  },
+  {
+    "revision": "95ea3e3f8ba42237823f",
+    "url": "/popup-widget/static/js/main.95ea3e3f.chunk.js"
+  },
+  {
+    "revision": "a93cec8a557ee382682a",
+    "url": "/popup-widget/static/js/1.a93cec8a.chunk.js"
+  },
+  {
+    "revision": "95ea3e3f8ba42237823f",
+    "url": "/popup-widget/static/css/main.ca501367.chunk.css"
+  },
+  {
+    "revision": "be9e34895c2c4fca5e94dbb6ab9df974",
+    "url": "/popup-widget/index.html"
+  }
+];
